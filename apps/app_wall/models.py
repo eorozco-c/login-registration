bebd152f7086a56f3307d8740025268e94b72cd7
Ernesto Orozco C <ernestorozco94@gmail.com>
2021-08-12 21:55:07 -0400

    CRUD WITH AJAX

--- apps/app_wall/models.py
+++ apps/app_wall/models.py
@@ -18,16 +18,25 @@ class Message(models.Model):
     objects = MessageManager()
 
 class  CommentManager(models.Manager):
+    def validate_comments_delete(self,id):
+        errors = {}
+        try:
+            comentario = self.get(id=id)
+            fecha_creacion = comentario.created_at
+            fecha_actual = datetime.now()
+            diferencia = fecha_actual - fecha_creacion
+            diferenciaInt = diferencia.total_seconds() / 60
+            if diferenciaInt > 30:
+                errors["datetime"] = "No puedes eliminar un mensaje despues de haberlo creado hace mas de 30 minutos"
+        except:
+            comentario = None
+        return errors
     def validate_comments(self,postData):
         errors = {}
-        comentario = self.get(id=postData)
-        fecha_creacion = comentario.created_at
-        fecha_actual = datetime.now()
-        diferencia = fecha_actual - fecha_creacion
-        diferenciaInt = diferencia.total_seconds() / 60
-        if diferenciaInt > 30:
-            errors["datetime"] = "No puedes eliminar un mensaje despues de haberlo creado hace mas de 30 minutos"
+        if postData["comment_message"] == "":
+            errors["comment_message"] = "Comentario Vacio"
         return errors
+            
 
 
 
--- apps/app_wall/views.py
+++ apps/app_wall/views.py
@@ -2,7 +2,9 @@ from django.http.response import JsonResponse
 from django.contrib import messages
 from django.shortcuts import render, redirect
 from .models import User,Message,Comment
-
+from datetime import datetime
+import locale
+locale.setlocale(locale.LC_TIME, "es_cl") 
 # Create your views here.
 def wall(request):
     if request.method == "GET":
@@ -10,7 +12,7 @@ def wall(request):
             idusuario = request.session["id"]
             context = {
                 "usuario" : User.objects.get(id=idusuario),
-                "message_users" : Message.objects.all(),
+                "message_users" : Message.objects.all().order_by("-id"),
                 "comments" : Comment.objects.all(),
             }
             return render(request, "wall.html", context)
@@ -21,42 +23,64 @@ def create_message(request):
         if "id" in request.session:
             errors = Message.objects.validate_messages(request.POST)
             if len(errors) > 0:
-                for key, value in errors.items():
-                    messages.error(request, value)
-                return redirect("/wall")
+                return JsonResponse(errors)
             idusuario = request.session["id"]
             usuario = User.objects.get(id=idusuario)
             mensaje = request.POST["create_message"]
-            Message.objects.create(user=usuario,message=mensaje)
-            return redirect("/wall")
+            new_message = Message.objects.create(user=usuario,message=mensaje)
+            data = {
+                "id" : new_message.id,
+                "message" : new_message.message,
+                "first_name" : new_message.user.first_name,
+                "last_name" : new_message.user.last_name,
+                "created_at" : datetime.strftime(new_message.created_at, "%d de %B de %Y a las %H:%M")
+            }
+            return JsonResponse(data)
     return redirect("/")
 
 def comment_message(request, idMesagge):
     if request.method == "POST":
         if "id" in request.session:
+            #print(request.POST["comment_message"])
+            errors = Comment.objects.validate_comments(request.POST)
+            if len(errors) > 0:
+                errors["idMesagge"] = idMesagge
+                return JsonResponse(errors)
             idusuario = request.session["id"]
             usuario = User.objects.get(id=idusuario)
             try:
                 mensaje = Message.objects.get(id=idMesagge)
             except:
-                return redirect("/wall")
+                return JsonResponse({"errors": "No se puede comentar en esta publicacion"})
             comentario = request.POST["comment_message"]
-            Comment.objects.create(user=usuario,message=mensaje,comment=comentario)
-            return redirect("/wall")
+            new_commentario = Comment.objects.create(user=usuario,message=mensaje,comment=comentario)
+            data = {
+                "id" : new_commentario.id,
+                "idMessage" : new_commentario.message.id,
+                "message" : new_commentario.message.message,
+                "first_name" : new_commentario.user.first_name,
+                "last_name" : new_commentario.user.last_name,
+                "comment" : new_commentario.comment,
+                "created_at" : datetime.strftime(new_commentario.created_at, "%d de %B de %Y a las %H:%M")
+            }
+            return JsonResponse(data)
     return redirect("/")
 
 def comment_delete(request,idComment):
     if request.method == "GET":
         if "id" in request.session:
-            errors = Comment.objects.validate_comments(idComment)
+            errors = Comment.objects.validate_comments_delete(idComment)
             if len(errors) > 0:
-                for key, value in errors.items():
-                    messages.error(request, value)
+                errors["idComment"] = idComment
                 return redirect("/wall")
             try:
                 del_comment = Comment.objects.get(id=idComment)
+                id_comment = del_comment.id
                 del_comment.delete()
             except:
                 del_comment = None
+                errors["idComment"] = idComment
+                return redirect("/wall")
+            print(id_comment)
             return redirect("/wall")
     return redirect("/")
